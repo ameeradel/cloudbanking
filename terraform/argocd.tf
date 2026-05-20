e396335bd26fdf8c38e7ce8f5a73388ec2777ffb
Ameer Adel <65094003+ameeradel@users.noreply.github.com>
2026-05-20 08:28:17 +0300

    Feature/helm backend chart (#8)
    * Add Helm chart for backend deployment
    
    * Add backend CI pipeline for ECR image builds
    
    * Add ArgoCD GitOps configuration

---
+++ terraform/argocd.tf
@@ -0,0 +1,22 @@
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+
+  values = [
+    yamlencode({
+      server = {
+        service = {
+          type = "ClusterIP"
+        }
+      }
+    })
+  ]
+}
